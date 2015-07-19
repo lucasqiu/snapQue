e84e77f8dfaaefa5c48a36bc5fb0f15c3daaf69a
Michelle Chan <michelle.chan@runscope.com>
2015-07-19 01:27:31 -0700

    the add table page

---
+++ snapQue/AddTableViewController.m
@@ -0,0 +1,40 @@
+//
+//  AddTableViewController.m
+//  snapQue
+//
+//  Created by Michelle Chan on 7/19/15.
+//  Copyright (c) 2015 Shi Qiu. All rights reserved.
+//
+
+#import "AddTableViewController.h"
+
+@interface AddTableViewController ()
+
+@end
+
+@implementation AddTableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue {
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
